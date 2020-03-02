17c6f33b1c510a357a21dea646da1495f60a4606
loydfassi <loyd.fassi@gmail.com>
2020-03-02 19:29:33 +0100

    add weather command

---
+++ commands/weather.js
@@ -0,0 +1,28 @@
+const config = require("../config.json");
+const fetch = require('node-fetch');
+module.exports = {
+    name: 'weather',
+    description: 'Donne la météo !',
+    guildOnly: false,
+    usage: '[ville]',
+    cooldown: 10,
+    async execute(message, args) {
+        const query = args.length === 1 ? `https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&lang=fr&appid=${config.weatherToken}` : `https://api.openweathermap.org/data/2.5/weather?q=${args[0]},${args[1]}&units=metric&lang=fr&appid=${config.weatherToken}`
+       const response = await fetch(query)
+       const parsed = await response.json()
+       console.log(parsed.weather);
+       
+       const descr = parsed.weather[0].description
+       const name = parsed.name
+       const temp = parsed.main.temp
+       const feelsLike = parsed.main.feelsLike
+       console.log(descr);
+       
+       message.reply(`
+        Ville: ${name}
+        Météo : ${descr}
+        Temperature : ${temp} degrés
+       `)
+        
+    },
+};

67c70bcda3b8b587ae1adfa5c3d22cd6a9511c9c
loydfassi <loyd.fassi@gmail.com>
2020-03-10 18:56:46 +0100

    add a better error handling on some commands

--- src/events/eventsListeners/ready.ts
+++ src/events/eventsListeners/ready.ts
@@ -3,8 +3,8 @@ import { IClient } from "../../../interfaces";
 const ready = (client: IClient): void => {
     client.on('ready', (): void => { // define action on bot ready
         // tslint:disable-next-line: no-console
-        console.log(`Je suis là !!`);
-        client.user.setActivity(`Javascript`);
+        console.log(`Ready`);
+        client.user.setActivity(`Casser des gueules`);
     });
 }
 module.exports = ready
